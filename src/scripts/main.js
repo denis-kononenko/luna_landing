fc98ed9b92456e3c664648d4eddd82c6449f761b
Denis Kononenko <deniskononenko88@gmail.com>
2023-11-30 11:35:00 +0200

    add burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,20 @@ const previousButton = document.querySelector('.features__previous-button');
 const nextButton = document.querySelector('.features__next-button');
 const activeSlideBlock = document.querySelector('.features__active-slide');
 const form = document.querySelector('.get-in-touch__form');
+const menu = document.querySelector('.menu');
+const menuOpener = document.querySelector('.header__menu-opener');
+const menuCross = document.querySelector('.menu__cross');
+const menuLinks = menu.querySelectorAll('a');
 let activeSlide = 0;
 
 const moveSlides = () => {
   slides.style.transform = `translateX(-${33.33 * activeSlide}%)`;
   activeSlideBlock.innerText = `0${activeSlide + 1}`;
+};
+
+const closeMenu = () => {
+  menu.classList.remove('menu--open');
+  document.body.classList.remove('page__body--menu-opened');
 };
 
 previousButton.addEventListener('click', () => {
@@ -44,3 +53,12 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   form.reset();
 });
+
+menuOpener.addEventListener('click', () => {
+  menu.classList.add('menu--open');
+  document.body.classList.add('page__body--menu-opened');
+});
+
+menuCross.addEventListener('click', closeMenu);
+
+menuLinks.forEach(link => link.addEventListener('click', closeMenu));
